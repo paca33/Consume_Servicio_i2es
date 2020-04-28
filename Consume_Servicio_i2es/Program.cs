bad38cfe56d2631e35b8e292233276d3c3a0bb52
Pablo Cabrera <pcabrera@aluminios.com>
2020-04-28 20:34:11 -0300

    Agregar archivos de proyecto.

---
+++ Consume_Servicio_i2es/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Runtime.InteropServices;
+
+
+
+
+namespace Consume_Servicio_i2es
+{
+    class Program
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        static void Main(string[] args)
+        {
+           // try
+            //{
+                string ConString = "Data Source=10.25.1.237;Initial Catalog=Nodum_Web;Trusted_Connection=True;Connection Timeout=30;";//ini.IniReadValue("Parametros", "Base");
+
+
+
+                    using (con = new SqlConnection(ConString))
+                {
+                    con.Open();
+                    //Vista pedidos Estado
+                    string CmdString = "Select * FROM vista_pedido_estado_articulos_web_xml";//ini.IniReadValue("Parametros", "sql" + i.ToString().Trim()); //
+                    cmd = new SqlCommand(CmdString, con);
+                     SqlDataReader reader = cmd.ExecuteReader();
+
+                    reader.Read();
+                    string xml = reader.GetString(0);
+                    Pedidos_Estado.ws_estados_pedidos wsConsumo_Estado = new Pedidos_Estado.ws_estados_pedidos();
+
+
+               
+              var pp=   wsConsumo_Estado.Callws_estados_pedidos("WAluminios", "ALURG12677", xml);
+
+
+
+                }
+            //}
+            //catch
+            //{
+
+            //}
+            
+        }
+    }
+}
+
---
+++ Consume_Servicio_i2es/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Consume_Servicio_i2es")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Consume_Servicio_i2es")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("8ae6fb1d-4508-4543-aa40-dd63a8a1f897")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
